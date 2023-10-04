1f06f518b96b95c08833876ad237c2b987ce2648
Cheng Tan <reg_eax@outlook.com>
2023-10-04 23:04:30 +0800

    Main done

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+
 import androidx.appcompat.app.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,31 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnJump = (Button) findViewById(R.id.btn_jump);
-        btnJump.setOnClickListener(new View.OnClickListener() {
+        Button btnJumpToOther = findViewById(R.id.btn_jumpToOther);
+        btnJumpToOther.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final AlertDialog.Builder normalDialog =
                         new AlertDialog.Builder(MainActivity.this);
                 normalDialog.setTitle("Continue Jump to the Other Activity");
                 normalDialog.setPositiveButton("YES",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                                startActivity(intent);
-                            }
+                        (dialog, which) -> {
+                            Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                            startActivity(intent);
                         });
                 normalDialog.setNegativeButton("NO",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
-                            }
-                        });
-                // display the AlertDialog
-                normalDialog.show();
+                        (dialog, which) -> { /* do nothing */ });
+                normalDialog.show(); // display the AlertDialog
             }
+        });
+
+        Button btnJumpToCounter = findViewById(R.id.btn_jumpToCounter);
+        btnJumpToCounter.setOnClickListener(view -> {
+            // TODO Counter
         });
     }
 }
--- app/src/main/java/com/example/helloandroid/OtherActivity.java
+++ app/src/main/java/com/example/helloandroid/OtherActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity extends AppCompatActivity {
+public class OtherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_other);
     }
 }
