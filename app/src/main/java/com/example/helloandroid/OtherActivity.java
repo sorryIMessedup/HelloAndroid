0bc2120be79bd65f068f9b98c3cc89498860b327
Cheng Tan <reg_eax@outlook.com>
2023-10-04 23:28:04 +0800

    Other done

--- app/src/main/java/com/example/helloandroid/OtherActivity.java
+++ app/src/main/java/com/example/helloandroid/OtherActivity.java
@@ -1,6 +1,10 @@
 package com.example.helloandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,19 @@ public class OtherActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        TextView btnText = findViewById(R.id.btn_showOrCloseText);
+        TextView otherText = findViewById(R.id.textview_other);
+
+        Button showOrCloseText = findViewById(R.id.btn_showOrCloseText);
+        showOrCloseText.setOnClickListener(view -> {
+            if (btnText.getText() == "SHOW") {
+                btnText.setText("HIDE");
+                otherText.setVisibility(View.VISIBLE);
+            } else {
+                btnText.setText("SHOW");
+                otherText.setVisibility(View.GONE);
+            }
+        });
     }
 }
